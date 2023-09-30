102a411e84eeca83590a72016d3f0192bce9bd5a
rahmat rafii <rahmatrafiindrayani@gmail.com>
2023-09-30 12:00:37 +0700

    coba 1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["https://rahmatrafiindrayani.vercel.app"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
